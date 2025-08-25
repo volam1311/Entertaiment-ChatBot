b14fd80acd35036ffc3ab3b0be9d006c65c6c08d
Lam <vophuclam1311@gmail.com>
2025-08-25 18:14:33 +1000

    Config backend file - Manage OPENAI API

---
+++ packages/server/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import type {Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/',(req : Request, res: Response) =>
+{
+    res.send();
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
