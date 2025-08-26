19e0f09bbd65ff6a3cdbc42c018170fcf8e8e7e1
Lam <vophuclam1311@gmail.com>
2025-08-26 14:33:47 +1000

    connect the front end and back end

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,6 +11,10 @@ app.get('/',(req : Request, res: Response) =>
 {
     res.send();
 })
+app.get('/api/hello',(req : Request, res: Response) =>
+{
+    res.json({message: 'Hello World'});
+})
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
